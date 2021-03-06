92b70c724b5e6083ed4c711fc29f5c0ff4f814a6
gurleenMaini <73643543+gurleenMaini@users.noreply.github.com>
2021-03-06 20:09:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,70 @@
-var bground, backgroundImage;
-var balloon, balloonImage;
-var myDataBase, databaseRef;
-var position;
+//variables
+var bg,backgroundImage; 
+var balloon, position;
+var database;
 
 function preload(){
-backgroundImage = loadImage("./Hot Air Ballon-01.png")
-balloonImage = loadAnimation("./Hot Air Ballon-02.png","./Hot Air Ballon-03.png","./Hot Air Ballon-04.png")
+  bg = loadImage("Hot Air Ballon-01.png");
+  backgroundImage = loadAnimation("Hot Air Ballon-02.png", "Hot Air Ballon-03.png", "Hot Air Ballon-04.png");
+
 }
-
 function setup() {
-  createCanvas(800,400);
-  bground= createSprite(400, 200, 800, 400);
-  bground.addImage("city",backgroundImage);
-  bground.scale=0.35
-  balloon=createSprite(100,300,20,20);
-  balloon.addAnimation("fly",balloonImage);
-  balloon.scale=0.3
+  //creating a canvas
+  createCanvas(800,500);
+  database = firebase.database();
 
-  myDataBase= firebase.database();
-  databaseRef= myDataBase.ref("bodyposition");
-  databaseRef.on("value",readPosition,problem);
+  var balloonPosition = database.ref("balloon/position");
+  balloonPosition.on("value", readHeight, showError);
+
+  bgImage= createSprite(0,0,50,50);
+  bgImage.addImage(bg);
+
+  //creating a balloon sprite and adding animation
+  balloon = createSprite(100, 300, 50, 50);
+  balloon.addAnimation("moving",backgroundImage);
+  balloon.scale= 0.32;
+
 }
 
 function draw() {
-  background(255,255,255);  
-  if (keyDown(UP_ARROW)){
-    changeposition(0,-1)
-    balloon.scale=balloon.scale-0.0009
-  }
-  if (keyDown(LEFT_ARROW)){
-    changeposition(-1,0)
-  }
-  if(keyDown(RIGHT_ARROW)){
-    changeposition(1,0)
-  }
-  if (keyDown(DOWN_ARROW)){
-    changeposition(0,1)
-    balloon.scale=balloon.scale+0.0009
-  }
-  drawSprites();
-}
-function changeposition(x,y){
-  balloon.x = balloon.x + x;
-  balloon.y = balloon.y + y;
-}
-function readPosition(data){
-  position=data.val()
-  balloon.x=position.x
-  balloon.y=position.y
+  background(0);  
 
-}
-function problem(){
-  console.log("error in your database")
-}
-function writePosition(x,y){
-  myDataBase.ref("balloon/position").set({
-      "x": position.x+ x, 
-      "y":position.y+ y
-  })
+  drawSprites();
+
+  //writing the position on the database, making the balloon move with arrow keys
+  //changing scale of the balloon when it goes up or down
+  if (keyDown(UP_ARROW)){
+    balloon.scale=0.25
+   updateHeight(0,-1);
   }
+  if (keyDown(RIGHT_ARROW)){
+    updateHeight(1,0);
+    }
+    if (keyDown(LEFT_ARROW)){
+      updateHeight(-1,0);
+      }
+      if (keyDown(DOWN_ARROW)){
+        updateHeight(0,+1);
+        }
+ }
+
+//function to read the position
+function readHeight(data){
+  position = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y;
+  console.log(position);
+}
+
+//function to show a string in the console if there is an error
+function showError(){
+  console.log("Error");
+}
+
+//function to write the position onto the database
+function updateHeight(x, y){
+  database.ref("balloon/position").set({
+    x : position.x + x,
+    y : position.y + y
+  })
+}
